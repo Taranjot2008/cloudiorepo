1ef7eb8bc5bebacd80ec6d3f50bc013cb7e23531
Taranjot Singh <taranjotscience@gmail.com>
2026-09-16 07:16:02 -0300

    Fixed: client url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://cloudio-user.onrender.com/",
   methods: ["GET", "POST"],
   credentials: true
 }));
